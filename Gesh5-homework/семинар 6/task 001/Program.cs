882ee3c08a7efbbc3dd9eddc992afd4c89b93018
Gesh5 <129482161+Gesh5@users.noreply.github.com>
2023-04-30 22:15:33 +1000

    Add files via upload

---
+++ Gesh5-homework/семинар 6/task 001/Program.cs
@@ -0,0 +1,115 @@
+﻿// Пользователь вводит с клавиатуры M чисел. 
+// посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+
+// через строку вводим числа, и присваиваем и заводим в массив
+// --------------------------------------------------------------
+
+// Console.Write("Введите числа через пробел: ");
+// int[] numbers = StringToNum(Console.ReadLine());
+// PrintArray(numbers);
+// int sum = 0;
+// for (int i = 0; i < numbers.Length; i++)
+// {
+//     if (numbers[i] > 0)
+//     {
+//         sum++;
+//     }
+// }
+// Console.WriteLine();
+// Console.WriteLine($"количество значений больше 0 = {sum}");
+
+
+// int[] StringToNum(string input)
+// {
+//     int count = 1;
+//     for (int i = 0; i < input.Length; i++)
+//     {
+//         if (input[i] == ',')
+//         {
+//             count++;
+//         }
+//     }
+
+//     int[] numbers = new int [count];
+//     int index = 0;
+
+//     for (int i = 0; i < input.Length; i++)
+//     {
+//         string temp = "";
+
+//         while (input [i] != ',')
+//         {
+//         if(i != input.Length - 1)
+//         {
+//             temp += input [i].ToString();
+//             i++;
+//         }
+//         else
+//         {
+//             temp += input [i].ToString();
+//             break;
+//         }
+//         }
+//         numbers[index] = Convert.ToInt32(temp);
+//         index++;
+//     }
+//     return numbers;
+// }
+
+
+// void PrintArray(int[] array)
+// {
+//     Console.Write("[ ");
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write(array[i] + " ");
+//     }
+//     Console.Write("]");
+// }
+
+//-------------------------------------------------
+
+// задаем массив случайных значений и внем считаем положительные числа
+
+// --------------------------------------------------------------
+
+Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
+Console.WriteLine("массив: ");
+PrintArray(numbers);
+int sum = 0; 
+
+ for (int i = 0; i < numbers.Length; i++)
+ {
+    if (numbers[i] > 0)
+    {
+        sum++;
+    }
+}
+Console.WriteLine();
+Console.WriteLine($"количество значений больше 0 = {sum}");
+
+
+void FillArrayRandomNumbers(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(-10, 100);
+    }
+}
+void PrintArray(int[] numbers)
+{
+    Console.Write("[");
+        for(int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+    Console.Write("]");
+}
---
+++ Gesh5-homework/семинар 6/task 002/Program.cs
@@ -0,0 +1,44 @@
+﻿// // // Напишите программу, которая найдёт точку пересечения двух прямых, 
+// // заданных уравнениями y = k1 * x + b1 y = k2 * x + b2
+//  b1, k1, b2 и k2 задаются пользователем.
+// // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+
+
+// y = k1 * x + b1 
+// y = k2 * x + b2
+
+// y-y = k1x + b1 - k2x + b2
+// y = k2x + b2
+
+// 0=k1-k2x +b1-b2
+// y = k2x + b2
+
+// k1-k2x = b1-b2
+// y = k2x + b2
+
+// x = b1-b2/k1-k2
+// y = k2x + b2
+
+// x = b1-b2/k1-k2 ---- double x
+
+// y = k2 *(b1-b2/k1-k2) + b2 ---- double y
+
+
+
+
+Console.WriteLine("введите k1 ");
+int k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите b1 ");
+int b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите k2 ");
+int k2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите b2 ");
+int b2 = Convert.ToInt32(Console.ReadLine());
+
+double x  = (b1-b2)/(k1-k2);
+double y = k2 * x + b2;
+
+
+Console.WriteLine($"точки пересечения двух прямых X: {x}, Y: {y}");
+
