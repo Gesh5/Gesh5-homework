5949bfe13f4c878e66b6208a788f96b334ccb606
Gesh5 <129482161+Gesh5@users.noreply.github.com>
2023-05-30 17:36:15 +1000

    Add files via upload

---
+++ Gesh5-homework/семинар 9/task 2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+
+void PrintNumbersSum(int m, int n, int sum)
+{
+    if (m > n)
+    {
+        Console.WriteLine($"сумма элементов от M до N = {sum}");
+        return;
+    }
+    sum = sum + (m++);
+    PrintNumbersSum(m, n, sum);
+}
+Console.Write("Введите число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+PrintNumbersSum(m, n, 0);
